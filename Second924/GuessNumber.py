77d082282a80c47052937ab4db828d41d9dd1ccd
PinmogeJoshua <138078951+PinmogeJoshua@users.noreply.github.com>
2024-10-07 21:07:30 +0800

    Update GuessNumber.py

--- Second924/GuessNumber.py
+++ Second924/GuessNumber.py
@@ -1,16 +1,28 @@
 import random
-count = 0
+
+# 初始化猜测次数
+guess_count = 0
+
+# 生成一个随机数
+num = random.randint(1, 100)
 
 while True:
-    num = random.randint(1,100)
-    number = int(input("Please guess one number:"))
-    if num == number:
-        print("Right! You made it!")
-        break
-    elif num > number:
-        print("A little bit smaller, please try it again")
-        count += 1
-    elif num < number:
-        print("A little bit bigger, please try it again")
-        count += 1
-print(f"你总共猜了{count}次")
+    try:
+        # 获取用户输入的数字
+        number = int(input("Please guess one number: "))
+        
+        # 比较用户输入的数字和随机数
+        if num == number:
+            print("Right! You made it!")
+            break
+        elif num > number:
+            print("A little bit smaller, please try it again")
+        elif num < number:
+            print("A little bit bigger, please try it again")
+        
+        # 增加猜测次数
+        guess_count += 1
+    except ValueError:
+        print("输入无效，请输入一个数字")
+
+print(f"你总共猜了 {guess_count} 次")
